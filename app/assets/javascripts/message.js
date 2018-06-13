e7c2a0449469e147709671edd76a430c6a4b1abb
tepepe61 <akimiatepp61@gmail.com>
2018-06-13 11:39:48 +0900

    メッセージ送信機能と非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,53 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image){var image  = `<img src ="${message.image}">`;
+  } else{
+    var image = '';}
+    var html = `<div class="chat">
+                  <div class="chat__memmber">
+                    ${message.name}
+                  </div>
+                  <div class="chat__memmber--time">
+                    ${message.created_at}
+                  </div>
+                  <div class="chat__message">
+                    <p class="chat__message__content">
+                      ${message.content}
+                    </p>
+                  </div>
+                  <div class="chat__message__image">
+                    ${image}
+                  </div>
+                </div>`
+                return html;
+    }
+    $(function() {
+    $('.chat')
+   });
+  $(function(){
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "post",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messageform').val('')
+        $('.form__submit').prop('disabled', false);
+        $('#new_message')[0].reset();
+        $('.body').animate({scrollTop:$('.body')[0].scrollHeight},'slow');
+       })
+      .fail(function(){
+       alert('error');
+     })
+    })
+  });
+});
